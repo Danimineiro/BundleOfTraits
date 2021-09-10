0a57f71d531ff62c8b3a4fc4bb77676783f5e20c
Daniel Plaster <daniel_plaster@live.de>
2021-09-10 21:15:51 +0200

    Initial Commit

---
+++ src/More Traits/More Traits/BOTThoughtPatches.cs
@@ -0,0 +1,86 @@
+﻿using Verse;
+using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse.AI;
+
+namespace More_Traits
+{
+	[StaticConstructorOnStartup]
+	class BOTThoughtPatches
+	{
+		static BOTThoughtPatches()
+		{
+			var harmony = new Harmony("dani.BOT.patch");
+			harmony.PatchAll();
+		}
+	}
+
+	[HarmonyPatch(typeof(PawnDiedOrDownedThoughtsUtility), "AppendThoughts_ForHumanlike")]
+	class PacifistWitnessDeathPatch
+	{
+		public static void Postfix(Pawn victim, DamageInfo? dinfo, PawnDiedOrDownedThoughtsKind thoughtsKind, List<IndividualThoughtToAdd> outIndividualThoughts, List<ThoughtToAddToAll> outAllColonistsThoughts)
+		{
+			bool flag = dinfo != null && dinfo.Value.Def.execution;
+			if (thoughtsKind == PawnDiedOrDownedThoughtsKind.Died && !flag)
+			{
+				foreach (Pawn pawn2 in PawnsFinder.AllMapsCaravansAndTravelingTransportPods_Alive)
+				{
+					if (pawn2 != victim && pawn2.needs != null && pawn2.needs.mood != null && (pawn2.MentalStateDef != MentalStateDefOf.SocialFighting || ((MentalState_SocialFighting)pawn2.MentalState).otherPawn != victim))
+					{
+						if (ThoughtUtility.Witnessed(pawn2, victim))
+						{
+							if (pawn2.story.traits.HasTrait(BOTDefOf.BOT_Pacifist))
+							{
+								outIndividualThoughts.Add(new IndividualThoughtToAdd(BOTDefOf.BOT_WittnessedDeathPacifist, pawn2, null, 1, 1));
+
+								if (pawn2 == (Pawn)dinfo.Value.Instigator)
+								{
+									outIndividualThoughts.Add(new IndividualThoughtToAdd(BOTDefOf.BOT_Pacifist_KilledHuman, pawn2, null, 1, 1));
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(PawnDiedOrDownedThoughtsUtility), "AppendThoughts_Relations")]
+	class PacifistKilledPatch
+	{
+		public static void Postfix (Pawn victim, DamageInfo? dinfo, PawnDiedOrDownedThoughtsKind thoughtsKind, List<IndividualThoughtToAdd> outIndividualThoughts, List<ThoughtToAddToAll> outAllColonistsThoughts)
+		{
+			if (dinfo != null && thoughtsKind == PawnDiedOrDownedThoughtsKind.Died)
+			{
+                if (dinfo.Value.Instigator is Pawn pawn2 && pawn2 != victim && victim.needs != null)
+                {
+                    if (pawn2.story.traits.HasTrait(BOTDefOf.BOT_Pacifist) && victim.RaceProps.Animal)
+                    {
+                        outIndividualThoughts.Add(new IndividualThoughtToAdd(BOTDefOf.BOT_Pacifist_KilledAnimal, pawn2, null, 1, 1));
+                    }
+                }
+            }
+		}
+	}
+
+	[HarmonyPatch(typeof(Toils_Ingest), "FinalizeIngest")]
+	class EclecticIngestPatch
+	{
+		public static void Postfix(Pawn ingester, TargetIndex ingestibleInd)
+		{
+			Thing food = ingester.CurJob.GetTarget(ingestibleInd).Thing;
+			bool isMeal = food.HasThingCategory(ThingCategoryDefOf.FoodMeals);
+
+			if (ingester.needs.mood != null && isMeal)
+            {
+				int ingredients = food.TryGetComp<CompIngredients>().ingredients.Count;
+				ingredients = (ingredients > BOTDefOf.BOT_EclecticPalateAte.stages.Count) ? BOTDefOf.BOT_EclecticPalateAte.stages.Count : ingredients;
+				ingredients = (ingredients > 0) ? ingredients - 1 : 0;
+
+				ingester.needs.mood.thoughts.memories.TryGainMemory(ThoughtMaker.MakeThought(BOTDefOf.BOT_EclecticPalateAte, ingredients));
+			}
+		}
+	}
+}
---
+++ src/More Traits/More Traits/BOTThoughts.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using JetBrains.Annotations;
+using RimWorld;
+using Verse;
+
+namespace More_Traits
+{
+	public class ThoughtWorker_Misanthrope : ThoughtWorker
+	{
+		// Token: 0x060000F1 RID: 241 RVA: 0x0000BE6C File Offset: 0x0000A06C
+		protected override ThoughtState CurrentSocialStateInternal(Pawn p, Pawn otherPawn)
+		{
+			return p.RaceProps.Humanlike && otherPawn.RaceProps.Humanlike && RelationsUtility.PawnsKnowEachOther(p, otherPawn);
+		}
+	}
+
+	public class ThoughtWorker_IsCarryingWeapon : ThoughtWorker
+	{
+		protected override ThoughtState CurrentStateInternal(Pawn p)
+		{
+			return p.equipment.Primary != null;
+		}
+	}
+
+	public class BOT_ThoughtWorker_Hot : ThoughtWorker
+	{
+		protected override ThoughtState CurrentStateInternal(Pawn p)
+		{
+			float num = p.AmbientTemperature - p.GetStatValue(StatDefOf.ComfyTemperatureMax, true);
+
+			if (p.story.traits.DegreeOfTrait(BOTDefOf.BOT_Temperature_Love) == -1)
+			{
+				//Loves the heat
+				if (p.AmbientTemperature > 25f && num < 10f)
+                {
+					return ThoughtState.ActiveAtStage(4);
+				}
+				return false;
+			} else if (p.story.traits.DegreeOfTrait(BOTDefOf.BOT_Temperature_Love) == 1)
+			{
+				//Loves the cold
+			} else
+            {
+				return false;
+            }
+			if (num <= 0f)
+			{
+				return ThoughtState.Inactive;
+			}
+			int num2;
+			if (num < 10f)
+			{
+				num2 = 0;
+			}
+			else if (num < 20f)
+			{
+				num2 = 1;
+			}
+			else if (num < 30f)
+			{
+				num2 = 2;
+			}
+			else
+			{
+				num2 = 3;
+			}
+			if (ModsConfig.IdeologyActive && p.Ideo.HasPrecept(PreceptDefOf.Temperature_Tough))
+			{
+				num2 -= 2;
+			}
+			if (num2 >= 0)
+			{
+				return ThoughtState.ActiveAtStage(num2);
+			}
+			return ThoughtState.Inactive;
+		}
+	}
+
+	public class BOT_ThoughtWorker_Cold : ThoughtWorker
+	{
+		protected override ThoughtState CurrentStateInternal(Pawn p)
+		{
+			float num = p.GetStatValue(StatDefOf.ComfyTemperatureMin, true) - p.AmbientTemperature;
+
+			if (p.story.traits.DegreeOfTrait(BOTDefOf.BOT_Temperature_Love) == -1)
+			{
+				//Loves the heat
+			}
+			else if (p.story.traits.DegreeOfTrait(BOTDefOf.BOT_Temperature_Love) == 1)
+			{
+				//Loves the cold
+				if (p.AmbientTemperature < 5f && num < 10f)
+				{
+					return ThoughtState.ActiveAtStage(4);
+				}
+				return false;
+			}
+			else
+			{
+				return false;
+			}
+			if (num <= 0f)
+			{
+				return ThoughtState.Inactive;
+			}
+			int num2;
+			if (num < 10f)
+			{
+				num2 = 0;
+			}
+			else if (num < 20f)
+			{
+				num2 = 1;
+			}
+			else if (num < 30f)
+			{
+				num2 = 2;
+			}
+			else
+			{
+				num2 = 3;
+			}
+			if (ModsConfig.IdeologyActive && p.Ideo.HasPrecept(PreceptDefOf.Temperature_Tough))
+			{
+				num2 -= 2;
+			}
+			if (num2 >= 0)
+			{
+				return ThoughtState.ActiveAtStage(num2);
+			}
+			return ThoughtState.Inactive;
+		}
+	}
+
+	[DefOf]
+	public static class BOTDefOf
+	{
+		public static TraitDef BOT_Misanthrope;
+
+		public static TraitDef BOT_Dysgeusia;
+
+		public static TraitDef BOT_Eclectic_Palate;
+
+		public static TraitDef BOT_Pacifist;
+
+		public static TraitDef BOT_Narcoleptic;
+
+		public static TraitDef BOT_Pyrophobia;
+
+		public static TraitDef BOT_Nyctophobia;
+
+		public static TraitDef BOT_Sleepyhead;
+
+		public static TraitDef BOT_GunKata;
+
+		public static TraitDef BOT_Loves_Sleeping;
+
+		public static TraitDef BOT_Cynical;
+
+		public static TraitDef BOT_StrongBody;
+
+		public static TraitDef BOT_WeakBody;
+
+		public static TraitDef BOT_Metabolism;
+
+		public static TraitDef BOT_Temperature_Love;
+
+		public static ThoughtDef BOT_Pacifist_KilledAnimal;
+
+		public static ThoughtDef BOT_Pacifist_KilledHuman;
+
+		public static ThoughtDef BOT_WittnessedDeathPacifist;
+
+		public static ThoughtDef BOT_PyrophobiaHoldingIncendiary;
+
+		public static ThoughtDef BOT_EclecticPalateAte;
+	}
+}
