160b6d6d5d73209f0c582891b36a92cf5b231e50
Dani <daniel_plaster@live.de>
2024-03-11 12:28:43 +0100

    Sleep Patch changes

--- Source/BOTJobPatch.cs
+++ Source/BOTJobPatch.cs
@@ -7,9 +7,9 @@ using Verse.AI;
 namespace More_Traits
 {
 	[HarmonyPatch(typeof(Toils_LayDown), "LayDown")]
-	class LayDownToilPatch
+	internal static class LayDownToilPatch
 	{
-		public static void Postfix(Toil __result, TargetIndex bedOrRestSpotIndex, bool hasBed, bool lookForOtherJobs, bool canSleep = true, bool gainRestAndHealth = true, PawnPosture noBedLayingPosture = PawnPosture.LayingOnGroundNormal)
+		internal static void Postfix(Toil __result, TargetIndex bedOrRestSpotIndex, bool hasBed, bool lookForOtherJobs, bool canSleep = true, bool gainRestAndHealth = true, PawnPosture noBedLayingPosture = PawnPosture.LayingOnGroundNormal)
 		{
 			__result.AddFailCondition(delegate ()
 			{
@@ -64,56 +64,76 @@ namespace More_Traits
 	///		This class patches the startjob function to modify the behaviour of sleep related traits
 	/// </summary>
 	[HarmonyPatch(typeof(Pawn_JobTracker), "StartJob")]
-	class JobPatch
+	internal static class JobPatch
 	{
-		public static void Prefix(Pawn ___pawn, Job newJob, JobCondition lastJobEndCondition)
-		{
-			if (___pawn.RaceProps.Animal || ___pawn.story == null || ___pawn.story.traits == null) return;
+		internal static void Prefix(Pawn ___pawn, ref Job newJob)
+        {
+            if (___pawn.AnimalOrWildMan()) return;
 
-			BOTTraitsManager Manager = Current.Game.GetComponent<BOTTraitsManager>();
+            BOTTraitsManager Manager = Current.Game.GetComponent<BOTTraitsManager>();
+            AddPawnToSleepDictionary(___pawn, Manager, newJob);
 
-			//Save the amount of rest a pawn with the Loves_Sleep trait has in order to later calculate how much recreation they should gain from that
-			if (newJob.def == JobDefOf.LayDown && newJob.targetA.HasThing && newJob.targetA.Thing.GetType() == typeof(Building_Bed) && ___pawn.story.traits.HasTrait(BOTTraitDefOf.BOT_Loves_Sleeping))
-			{
-				if (!Manager.GetLoves_SleepDic().ContainsKey(___pawn))
-				{
-					Manager.GetLoves_SleepDic()[___pawn] = ___pawn.needs.rest.CurLevelPercentage;
-				}
+            //Only executed if the pawn was entered in here due to Job.LayDown in a Bed
+            //determines how much recreation a pawn should get
+            CalcSleepJoy(___pawn, Manager);
 
-				return;
-			}
+            //Deals with sleepyheads not wanting to wake up
+            MaybeContinueSleeping(___pawn, ref newJob, Manager);
+        }
 
-			//Only executed if the pawn was entered in here due to Job.LayDown in a Bed
-			//determines how much recreation a pawn should get
-			if (Manager.GetLoves_SleepDic().ContainsKey(___pawn))
-			{
-				float initialRestPercent = Manager.GetLoves_SleepDic()[___pawn];
-				float currentRestPercent = ___pawn.needs.rest.CurLevelPercentage;
-				float recreationGainPercent = (currentRestPercent - initialRestPercent) * 0.3f;
+        private static void MaybeContinueSleeping(Pawn pawn, ref Job newJob, BOTTraitsManager Manager)
+        {
+            if (!pawn.HasTrait(BOTTraitDefOf.BOT_Sleepyhead)) return;
+            if (!(pawn.CurJobDef == JobDefOf.LayDown || pawn.CurJobDef == JobDefOf.LayDownAwake)) return;
+            if (newJob.def == JobDefOf.LayDown || newJob.def == JobDefOf.LayDownAwake) return;
+            if (pawn.needs.rest.CurCategory != RestCategory.Rested || Manager.GetSleepyHeadSet().Contains(pawn)) return;
+            if (Rand.Value <= 0.3f) return;
+            
+            newJob = pawn.CurJob;
+            newJob.def = JobDefOf.LayDown;
+            newJob.forceSleep = true;
+            pawn.needs.rest.CurLevelPercentage = 0.30f;
+            pawn.TryGainMemory(BOTThoughtDefOf.BOT_SleepyHeadContinuesSleeping, 0);
+            Manager.GetSleepyHeadSet().Add(pawn);
+        }
 
-				___pawn.needs.joy.GainJoy(recreationGainPercent, BOTJoyKindDefOf.BOT_LovesSleepSleeping);
+        private static void CalcSleepJoy(Pawn ___pawn, BOTTraitsManager Manager)
+        {
+            if (!Manager.GetLoves_SleepDic().ContainsKey(___pawn)) return;
 
-				if (currentRestPercent > 0.9)
-				{
-					___pawn.TryGainMemory(BOTThoughtDefOf.BOT_LovesSleepWellRested, 0);
-				}
+            float initialRestPercent = Manager.GetLoves_SleepDic()[___pawn];
+            float currentRestPercent = ___pawn.needs.rest.CurLevelPercentage;
+            float recreationGainPercent = (currentRestPercent - initialRestPercent) * 0.3f;
 
-				Manager.GetLoves_SleepDic().Remove(___pawn);
-			}
+            ___pawn.needs.joy.GainJoy(recreationGainPercent, BOTJoyKindDefOf.BOT_LovesSleepSleeping);
 
-			//Deals with sleepyheads not wanting to wake up
-			if ((___pawn.CurJobDef == JobDefOf.LayDown || ___pawn.CurJobDef == JobDefOf.LayDownAwake) && newJob.def != JobDefOf.LayDown && ___pawn.story.traits.HasTrait(BOTTraitDefOf.BOT_Sleepyhead) && ___pawn.needs.rest.CurCategory == RestCategory.Rested && !Manager.GetSleepyHeadSet().Contains(___pawn))
-			{
-				if (Rand.Value > 0.3)
-				{
-					newJob = ___pawn.CurJob;
-					newJob.def = JobDefOf.LayDown;
-					newJob.forceSleep = true;
-					___pawn.needs.rest.CurLevelPercentage = 0.30f;
-					___pawn.TryGainMemory(BOTThoughtDefOf.BOT_SleepyHeadContinuesSleeping, 0);
-					Manager.GetSleepyHeadSet().Add(___pawn);
-				}
-			}
-		}
-	}
+            if (currentRestPercent > 0.9)
+            {
+                ___pawn.TryGainMemory(BOTThoughtDefOf.BOT_LovesSleepWellRested, 0);
+            }
+
+            Manager.GetLoves_SleepDic().Remove(___pawn);
+        }
+
+        /// <summary>
+        ///		Save the amount of rest a pawn with the Loves_Sleep trait has in order to later calculate how much recreation they should gain from that
+        /// </summary>
+        /// <param name="pawn"></param>
+        /// <param name="Manager"></param>
+        /// <param name="newJob"></param>
+        private static void AddPawnToSleepDictionary(Pawn pawn, BOTTraitsManager Manager, Job newJob)
+        {
+            if (newJob.def != JobDefOf.LayDown) return;
+            if (!newJob.targetA.HasThing) return;
+            if (newJob.targetA.Thing.GetType() != typeof(Building_Bed)) return;
+            if (!pawn.HasTrait(BOTTraitDefOf.BOT_Loves_Sleeping)) return;
+
+            if (!Manager.GetLoves_SleepDic().ContainsKey(pawn))
+            {
+                Manager.GetLoves_SleepDic()[pawn] = pawn.needs.rest.CurLevelPercentage;
+            }
+
+            return;
+        }
+    }
 }
