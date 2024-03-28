ce12a7c228d9373b2210b0c814acc7492bcb1776
Dani <daniel_plaster@live.de>
2024-03-28 16:32:05 +0100

    Make Saving World Vars possible

---
+++ Source/[DN] BOT 1.5/WorldComps/Saving.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace More_Traits.WorldComps
+{
+    public class Saving : WorldComponent
+    {
+        public Saving(World world) : base(world) { }
+
+        public static Saving Current => Verse.Current.Game.World.GetComponent<Saving>();
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+        }
+    }
+}
