5dda8cf9043c5b9ff7ad489bb17644137d115eca
Dani <daniel_plaster@live.de>
2024-03-28 14:30:12 +0100

    TryGainMemory Extension

---
+++ Source/[DN] BOT 1.5/Extensions/PawnExtensions.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace More_Traits.Extensions
+{
+    public static class PawnExtensions
+    {
+        public static void TryGainMemory(this Pawn pawn, ThoughtDef thoughtDef, int forcedLevel)
+        {
+            pawn.needs.mood?.thoughts.memories.TryGainMemory(ThoughtMaker.MakeThought(thoughtDef, forcedLevel));
+        }
+    }
+}
