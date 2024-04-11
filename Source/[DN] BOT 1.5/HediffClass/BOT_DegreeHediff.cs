a584fa67ac2589d555699784582716cd359d9817
Dani <daniel_plaster@live.de>
2024-04-12 01:30:50 +0200

    rework BOT hediff class to be more generic

--- Source/[DN] BOT 1.5/HediffClass/BOT_DegreeHediff.cs
+++ Source/[DN] BOT 1.5/HediffClass/BOT_DegreeHediff.cs
@@ -2,19 +2,30 @@
 using More_Traits.Extensions;
 using More_Traits.ModExtensions;
 using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace More_Traits.HediffClass
 {
     public class BOT_DegreeHediff : Hediff
     {
-        private int degreeCache = 0;
+        private BOT_HediffExtension extension;
         private TraitDef traitDef;
+
+        private int degreeCache = 0;
+
+        public BOT_HediffExtension Extension => extension ?? (extension = def.GetModExtension<BOT_HediffExtension>());
+
+        public override string Label => traitDef.DataAtDegree(degreeCache).label;
+
+        public override string Description => traitDef.DataAtDegree(degreeCache).description.Formatted(pawn.Named("PAWN")).AdjustedFor(pawn, "PAWN", true).Resolve();
+
+        public override Color LabelColor => Extension.stageColors?[degreeCache] ?? ColorLibrary.GrassGreen;
 
         public override void PostMake()
         {
             base.PostMake();
-            degreeCache = pawn.story.traits.GetTrait(traitDef = def.GetModExtension<BOT_HediffExtension>().traitDef).Degree;
+            degreeCache = pawn.story.traits.GetTrait(traitDef = Extension.traitDef).Degree;
         }
 
         public override int CurStageIndex => degreeCache;
@@ -26,6 +37,13 @@ namespace More_Traits.HediffClass
                 if (Find.TickManager.TicksGame % 300 != 0) return false;
                 return !pawn.HasTrait(traitDef);
             }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Defs.Look(ref traitDef, nameof(traitDef));
+            Scribe_Values.Look(ref degreeCache, nameof(degreeCache));
         }
     }
 }
