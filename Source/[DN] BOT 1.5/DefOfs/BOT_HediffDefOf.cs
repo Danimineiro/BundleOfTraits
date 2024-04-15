12cbdbd3eb78b15559878cde5af8d4f6693d5005
Dani <daniel_plaster@live.de>
2024-04-15 02:22:36 +0200

    implement pyrophobia

---
+++ Source/[DN] BOT 1.5/DefOfs/BOT_HediffDefOf.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace More_Traits.DefOfs
+{
+    [DefOf]
+    public static class BOT_HediffDefOf
+    {
+        static BOT_HediffDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(HediffDefOf));
+
+        public static HediffDef Burn;
+    }
+}
