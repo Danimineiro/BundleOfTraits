886d0f5573c982340adaa0a021da6d6bfba4f026
Dani <daniel_plaster@live.de>
2024-04-18 13:58:07 +0200

    updated Apathetic

---
+++ Source/[DN] BOT 1.5/TraitEdits/StartupTraitEditor.cs
@@ -0,0 +1,21 @@
+﻿using More_Traits.DefOfs;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace More_Traits.TraitEdits
+{
+    [StaticConstructorOnStartup]
+    public static class StartupTraitEditor
+    {
+        static StartupTraitEditor()
+        {
+            BOT_TraitDefOf.BOT_Apathetic.DataAtDegree(0).disallowedInspirations = DefDatabase<InspirationDef>.AllDefsListForReading;
+            BOT_TraitDefOf.BOT_Apathetic.conflictingPassions = DefDatabase<SkillDef>.AllDefsListForReading;
+        }
+    }
+}
