38f810544e8b79bc7dd39d790286a2353fe71b85
Dani <daniel_plaster@live.de>
2024-04-19 11:27:35 +0200

    Update EarlyBird Trait

---
+++ Source/[DN] BOT 1.5/ThoughtWorkers/ThoughtWorker_EarlyBirdTimeThought.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace More_Traits.ThoughtWorkers
+{
+    public class ThoughtWorker_EarlyBirdTimeThought : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            if (p.Awake() && GenLocalDate.HourInteger(p) >= 6 && GenLocalDate.HourInteger(p) < 13)
+            {
+                return ThoughtState.ActiveAtStage(0);
+            }
+            if (p.Awake() && GenLocalDate.HourInteger(p) >= 17)
+            {
+                return ThoughtState.ActiveAtStage(1);
+            }
+
+            return false;
+        }
+    }
+}
