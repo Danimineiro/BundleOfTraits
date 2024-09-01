2e95068d71bafa6902d01b6dfa2cc9d4a6addb3a
Dani <daniel_plaster@live.de>
2024-09-01 18:49:54 +0200

    Implement a function to retrieve the private array from a list

---
+++ Source/[DN] BOT 1.5/Extensions/ListExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace More_Traits.Extensions;
+
+public static class ListExtensions
+{
+    public static Span<T> AsSpanUnsafe<T>(this List<T> list)
+    {
+        return ListConverter.AsSpan(list);
+    }
+}
+
