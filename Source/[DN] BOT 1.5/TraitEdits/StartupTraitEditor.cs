e96f6ec8bd76460676a77ff8dc478ccd5597b917
Dani <daniel_plaster@live.de>
2024-10-30 22:34:07 +0100

    avoid linking the lists

--- Source/[DN] BOT 1.5/TraitEdits/StartupTraitEditor.cs
+++ Source/[DN] BOT 1.5/TraitEdits/StartupTraitEditor.cs
@@ -9,7 +9,7 @@ public static class StartupTraitEditor
 {
     static StartupTraitEditor()
     {
-        BOT_TraitDefOf.BOT_Apathetic.DataAtDegree(0).disallowedInspirations = DefDatabase<InspirationDef>.AllDefsListForReading;
-        BOT_TraitDefOf.BOT_Apathetic.conflictingPassions = DefDatabase<SkillDef>.AllDefsListForReading;
+        BOT_TraitDefOf.BOT_Apathetic.DataAtDegree(0).disallowedInspirations.AddRange(DefDatabase<InspirationDef>.AllDefsListForReading);
+        BOT_TraitDefOf.BOT_Apathetic.conflictingPassions.AddRange(DefDatabase<SkillDef>.AllDefsListForReading);
     }
 }
