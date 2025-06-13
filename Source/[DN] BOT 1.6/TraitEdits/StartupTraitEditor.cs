87e1176e7e4409e8cf92acfd2dd6d8433f48854b
Dani <daniel_plaster@live.de>
2025-06-13 12:29:20 +0200

    Adjust StartupTraitEditor for 1.6

--- Source/[DN] BOT 1.6/TraitEdits/StartupTraitEditor.cs
+++ Source/[DN] BOT 1.6/TraitEdits/StartupTraitEditor.cs
@@ -7,7 +7,7 @@ public static class StartupTraitEditor
 {
     static StartupTraitEditor()
     {
-        BOT_TraitDefOf.BOT_Apathetic.DataAtDegree(0).disallowedInspirations = new(DefDatabase<InspirationDef>.AllDefsListForReading);
-        BOT_TraitDefOf.BOT_Apathetic.conflictingPassions = new(DefDatabase<SkillDef>.AllDefsListForReading);
+        BOT_TraitDefOf.BOT_Apathetic.DataAtDegree(0).disallowedInspirations = [.. DefDatabase<InspirationDef>.AllDefsListForReading];
+        BOT_TraitDefOf.BOT_Apathetic.conflictingPassions = [.. DefDatabase<SkillDef>.AllDefsListForReading];
     }
 }
